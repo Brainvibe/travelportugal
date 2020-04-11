9d4478f6ceb273ed8fe0b9ba32491f0beace7222
Nuno Nunes <nuno.t.nunes@gmail.com>
2020-04-11 13:43:26 +0100

    clean up code, removed old comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
-/*
-//Fadein when page loads
-document.body.className = 'fade';
-
-document.addEventListener("DOMContentLoaded", function (e) {
-  document.body.className = '';
-});
-
-document.addEventListener("DOMContentLoaded", function (e) {
-  document.body.classList.remove('fade');
-});
-
-//services dropdown slide animation
-
-*/
 
 $('.owl-carousel').owlCarousel({
   stagePadding: 50,
