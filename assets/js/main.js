cb04acb838946618cc791f65c682b2ab5ec9eaa5
Nuno Nunes <nuno.t.nunes@gmail.com>
2020-04-15 20:29:27 +0100

    removed unused js file, cleaned code, fixed submit button color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//loading carousel and settings
 
 $('.owl-carousel').owlCarousel({
   stagePadding: 60,
@@ -19,3 +20,32 @@ $('.owl-carousel').owlCarousel({
 })
 
 
+//Function To Display Popup
+function div_show() {
+    document.getElementById('contact-container').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide() {
+    document.getElementById('contact-container').style.display = "none";
+}
+
+function sendEmail(contactForm) {
+    emailjs.send('gmail', 'travel_portugal', {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "information_feedback": contactForm.FormControlText.value
+        })
+        .then(
+            function (response) {
+
+                
+                alert("Thank you for your message.");
+                div_hide();
+            },
+            function (error) {
+                
+                alert("Please try again.");
+            }
+        );
+    return false;
+}
